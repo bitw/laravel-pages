c104acb56124fc7e4b6139de95a976eba5b40d37
bitw <endval@gmail.com>
2014-12-28 17:26:27 +0600

    disable soft delete in pages

--- src/Bitw/Pages/Models/Page.php
+++ src/Bitw/Pages/Models/Page.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Auth;
 class Page extends Node implements SluggableInterface
 {
 
-	use SoftDeletingTrait;
+	//use SoftDeletingTrait;
 	use SluggableTrait;
 	/**
 	 * Table name.
